0de21c61f07a585b17089a2377adf65d1973003a
Aiyetin Roy <aiyetinroy@gmail.com>
2022-04-30 00:48:11 +0100

    Initial Commit

---
+++ UpTide/UpTideApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpTideApp.swift
+//  UpTide
+//
+//  Created by Roy Aiyetin on 30/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct UpTideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
